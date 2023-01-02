7bca0a62f750f1d6dec93e918c4f267c7a0154ed
bitleand <gartear@proton.me>
2023-01-02 11:46:14 +0200

    added: app logic timer

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,5 +1,45 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
-class Timer extends Component {}
+class Timer extends Component {
+  state = {
+    count: 0,
+    setCounting: false,
+  };
+
+  handleStart = () => {
+    this.setState({ setCounting: true });
+
+    this.counterId = setInterval(() => {
+      this.setState({ count: this.state.count + 1 });
+    }, 1000);
+  };
+
+  handleStop = () => {
+    this.setState({ setCounting: false });
+
+    clearInterval(this.counterId);
+  };
+
+  handleReset = () => {
+    this.setState({ setCounting: false, count: 0 });
+
+    clearInterval(this.counterId);
+  };
+
+  render() {
+    return (
+      <section className="Timer">
+        <h1>React - Timer</h1>
+        <h3>{this.state.count}</h3>
+        {!this.state.setCounting ? (
+          <button onClick={this.handleStart}>Start</button>
+        ) : (
+          <button onClick={this.handleStop}>Stop</button>
+        )}
+        <button onClick={this.handleReset}>Reset</button>
+      </section>
+    );
+  }
+}
 
 export default Timer;
